57cb78db672c1afd81649627426364e76fcf3f58
Annie Chang <chun@Annie-Chang-2.local>
2026-09-15 07:51:12 +0900

    Wave 1 all done

--- tests/test_wave_01.py
+++ tests/test_wave_01.py
@@ -140,7 +140,7 @@ def test_adds_movie_to_non_empty_user_watchlist():
     assert movie in updated_data["watchlist"]
     assert FANTASY_2 in updated_data["watchlist"]
 
-@pytest.mark.skip()
+#@pytest.mark.skip()
 def test_moves_movie_from_watchlist_to_empty_watched():
     # Arrange
     janes_data = {
@@ -159,12 +159,13 @@ def test_moves_movie_from_watchlist_to_empty_watched():
     assert len(updated_data["watchlist"]) == 0
     assert len(updated_data["watched"]) == 1
     
-    raise Exception("Test needs to be completed.")
+    #raise Exception("Test needs to be completed.")
     # *******************************************************************************************
     # ****** Add assertions here to test that the correct movie was added to "watched" **********
+    assert updated_data["watched"][0]["title"] == MOVIE_TITLE_1
     # *******************************************************************************************
 
-@pytest.mark.skip()
+#@pytest.mark.skip()
 def test_moves_movie_from_watchlist_to_watched():
     # Arrange
     movie_to_watch = HORROR_1
@@ -183,9 +184,10 @@ def test_moves_movie_from_watchlist_to_watched():
     assert len(updated_data["watchlist"]) == 1
     assert len(updated_data["watched"]) == 2
     
-    raise Exception("Test needs to be completed.")
+    #raise Exception("Test needs to be completed.")
     # *******************************************************************************************
     # ****** Add assertions here to test that the correct movie was added to "watched" **********
+    assert updated_data["watched"][1] == movie_to_watch
     # *******************************************************************************************
 
 @pytest.mark.skip()
--- viewing_party/party.py
+++ viewing_party/party.py
@@ -18,6 +18,21 @@ def add_to_watchlist(user_data, movie):
     user_data["watchlist"].append(movie)
     return user_data
 
+def watch_movie(user_data, title):
+    for i in range(len(user_data["watchlist"])):
+        if isinstance(user_data, dict):
+            if title == user_data["watchlist"][i]["title"]:
+                user_data["watched"].append(user_data["watchlist"].pop(i))
+                return user_data
+                #print(user_data)
+        else:
+            if title == user_data["watchlist"][i]:
+                user_data["watchlist"].remove("title")
+                user_data["watched"].append("title")
+                return user_data
+
+    return user_data
+
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
 # -----------------------------------------
