a7dd131e65d3ba5281320c747b8faedf2e25d22c
Annie Chang <chun@Annie-Chang-2.local>
2026-09-15 07:11:13 +0900

    add_to_watched pone

--- tests/test_wave_01.py
+++ tests/test_wave_01.py
@@ -58,7 +58,7 @@ def test_create_no_rating_movie():
     # Assert
     assert new_movie is None
 
-@pytest.mark.skip()
+#@pytest.mark.skip()
 def test_adds_movie_to_user_watched():
     # Arrange
     movie = {
@@ -79,7 +79,7 @@ def test_adds_movie_to_user_watched():
     assert updated_data["watched"][0]["genre"] == GENRE_1
     assert updated_data["watched"][0]["rating"] == RATING_1
 
-@pytest.mark.skip()
+#@pytest.mark.skip()
 def test_adds_movie_to_non_empty_user_watched():
     # Arrange
     movie = {
--- viewing_party/party.py
+++ viewing_party/party.py
@@ -10,7 +10,9 @@ def create_movie(title, genre, rating):
     else:
         return None
 
-#def add_to_watched(user_data, movie):
+def add_to_watched(user_data, movie):
+    user_data["watched"].append(movie)
+    return user_data
 
 
 # -----------------------------------------
