f593faa063904ccec62623cf1c0b3f6d6788170b
Annie Chang <chun@Annie-Chang-2.local>
2026-09-15 07:17:41 +0900

    add_to_watchlist done

--- tests/test_wave_01.py
+++ tests/test_wave_01.py
@@ -99,7 +99,7 @@ def test_adds_movie_to_non_empty_user_watched():
     assert movie in updated_data["watched"]
     assert FANTASY_2 in updated_data["watched"]
 
-@pytest.mark.skip()
+#@pytest.mark.skip()
 def test_adds_movie_to_user_watchlist():
     # Arrange
     movie = {
@@ -120,7 +120,7 @@ def test_adds_movie_to_user_watchlist():
     assert updated_data["watchlist"][0]["genre"] == GENRE_1
     assert updated_data["watchlist"][0]["rating"] == RATING_1
 
-@pytest.mark.skip()
+#@pytest.mark.skip()
 def test_adds_movie_to_non_empty_user_watchlist():
     # Arrange
     movie = {
