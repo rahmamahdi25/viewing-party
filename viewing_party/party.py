0f27761b7512e187837a36627a6bd3bda981059d
Rahma Mahdi <rahmamahdi.25@gmail.com>
2026-09-15 07:11:44 -0700

    feat: Implement Wave 2 movie rating and genre functions

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -33,10 +33,42 @@ def watch_movie(user_data, title):
 
     return user_data
 
-# -----------------------------------------
 # ------------- WAVE 2 --------------------
-# -----------------------------------------
 
+def get_watched_avg_rating(user_data):
+    total_ratings = 0.0
+
+    watched = user_data.get("watched", [])
+    if not watched:
+        return total_ratings
+
+    for movie in watched:
+        rating = movie["rating"]
+        total_ratings += rating
+
+    avg_rating = total_ratings / len(watched)
+
+    return avg_rating
+
+def get_most_watched_genre(user_data):
+
+    if not user_data["watched"]:
+        return None
+    
+    genre_count = {}
+    max_count = -1
+    get_most_watched_genre = None
+
+    for movie in user_data["watched"]:
+        genre = movie["genre"]
+        genre_count[genre] = genre_count.get(genre, 0) + 1
+
+        if genre_count[genre] > max_count:
+            max_count = genre_count[genre]
+            get_most_watched_genre = genre
+
+
+    return get_most_watched_genre
 
 # -----------------------------------------
 # ------------- WAVE 3 --------------------
